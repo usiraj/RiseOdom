5102f443035a74c80809b4d9dd2fca1b079df141
Usama <osama@smme.edu.pk>
2016-02-20 17:19:33 +0500

    Added cmake

---
+++ FindRiseOdom.cmake
@@ -0,0 +1,24 @@
+# find sgslam
+FIND_PATH(RISEODOM_INCLUDE_DIR NAMES r/stereoodom_base.h
+  PATHS
+  ${RISEODOM_ROOT}/include
+  ${RISEODOM_ROOT}
+  /usr/include
+  /opt/local/include
+  /usr/local/include
+  /sw/include
+  NO_DEFAULT_PATH
+  )
+  
+FIND_LIBRARY(RISEODOM_LIBRARY NAMES riseodom
+  PATHS
+  ${RISEODOM_ROOT}/lib
+  /usr/lib
+  /usr/local/lib
+  /opt/local/lib
+  /sw/lib
+  )
+  
+find_package(OpenCV REQUIRED)
+set(RISEODOM_LIBRARY ${OpenCV_LIBS} ${RISEODOM_LIBRARY} )
+add_compile_options(-std=gnu++11)
